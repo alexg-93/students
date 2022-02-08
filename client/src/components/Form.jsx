17dab00c2c1cabbf30f006e4e90c7d5e16dd8563
Alex Gordienko <alexgordienko1993@gmail.com>
2022-02-08 06:44:11 +0200

    Update Form.jsx

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -38,7 +38,7 @@ const Form = () => {
   });
 
   const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://ortsudents.herokuapp.com/api',
 })
 
   const { vertical, horizontal, open } = state;
